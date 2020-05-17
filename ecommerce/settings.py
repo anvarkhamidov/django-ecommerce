076278aabaa0065950dddbba3fa9c8b21220ceeb
Anvar Khamidov <anvarbekkhamidov@yandex.com>
2020-05-17 15:31:36 +0500

    feat: UI improvements, pagination and custom template tags

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
     'core',
 ]
 
@@ -134,3 +135,8 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+# CRISPY FORMS
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
