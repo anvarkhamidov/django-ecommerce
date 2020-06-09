1389e7c2115939b6d25ccd6e48f46386a96332ed
Anvar Khamidov <anvarbekkhamidov@yandex.com>
2020-06-09 22:40:48 +0500

    feat: checkout and other changes

--- core/views.py
+++ core/views.py
@@ -5,10 +5,48 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, View
 from django.contrib import messages
 from django.utils import timezone
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
+from .forms import CheckoutForm
 
-def checkout(request):
-    return render(request, "checkout.html")
+
+class CheckoutView(View):
+    def get(self, *args, **kwargs):
+        form = CheckoutForm()
+        context = { 'form': form }
+        return render(self.request, 'checkout.html', context)
+
+    def post(self, *args, **kwargs):
+        form = CheckoutForm(self.request.POST or None)
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            if form.is_valid():
+                street_address = form.cleaned_data.get('street_address')
+                apartment_address = form.cleaned_data.get('apartment_address')
+                country = form.cleaned_data.get('country')
+                zipcode = form.cleaned_data.get('zipcode')
+                # todo implemeting this
+                # same_billing_address = form.cleaned_data.get('same_billing_address')
+                # save_info = form.cleaned_data.get('save_info')
+                payment_option = form.cleaned_data.get('payment_option')
+                billing_address = BillingAddress(
+                    user=self.request.user,
+                    street_address=street_address,
+                    apartment_address=apartment_address,
+                    country=country,
+                    zipcode=zipcode
+                )
+                billing_address.save()
+                order.billing_address = billing_address
+                order.save()
+                messages.info(self.request, "CheckoutForm is valid")
+                return redirect('core:checkout')
+            messages.warning(self.request, "Invalid data")
+            return redirect('core:checkout')
+            context = { 'object': order }
+            return render(self.request, 'order-summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order")
+            return redirect("core:order-summary")
 
 class HomeView(ListView):
     model = Item
@@ -20,9 +58,7 @@ class OrderSummaryView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
         try:
             order = Order.objects.get(user=self.request.user, ordered=False)
-            context = {
-                'object': order
-            }
+            context = { 'object': order }
             return render(self.request, 'order-summary.html', context)
         except ObjectDoesNotExist:
             messages.error(self.request, "You do not have an active order")
